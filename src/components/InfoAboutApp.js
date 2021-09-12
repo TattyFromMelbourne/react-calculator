27bdd3a7c249d262334d7072000c558c71ae9003
Amir <dev.amirhemmati1382@gmail.com>
2021-09-12 16:26:49 +0430

    format all files in src directory, edit script in package.json

--- src/components/InfoAboutApp.js
+++ src/components/InfoAboutApp.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
-import GitHubCorner from './GitHubCorner'
+import React, { Component } from 'react';
+import GitHubCorner from './GitHubCorner';
 
 class InfoAboutApp extends Component {
-  render() {
-    return (
-      <React.Fragment>
-
-        <div>
-          <GitHubCorner />
-          <h1 className="App-title">React Calculator</h1>
-          <div id='intro' className="App-intro">
-            Calculator built with React.js v16
-          </div>
-        </div>
-      </React.Fragment>
-  )}
+	render() {
+		return (
+			<React.Fragment>
+				<div>
+					<GitHubCorner />
+					<h1 className="App-title">React Calculator</h1>
+					<div id="intro" className="App-intro">
+						Calculator built with React.js v16
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default InfoAboutApp;
